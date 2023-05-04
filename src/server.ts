13a7494c21234490e2175b2df319e88aebb9308d
bruno-sisplan <bruno.sena@sisplansistemas.com.br>
2023-05-04 08:16:08 -0300

    iniciado desperdicio

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { authRoutes } from './routes/auth'
 import { entradaEstoqueRoutes } from './routes/movestoque';
 import { saidaEstoqueRoutes } from './routes/movestoque';
 import { vendaRoutes } from './routes/venda';
+import { desperdicioProdutoRoutes } from './routes/desperdicio';
 
 
 async function bootstrap() {
@@ -21,6 +22,7 @@ async function bootstrap() {
     await fastify.register(entradaEstoqueRoutes);
     await fastify.register(saidaEstoqueRoutes);
     await fastify.register(vendaRoutes);
+    await fastify.register(desperdicioProdutoRoutes);
 
     await fastify.listen({ port: 3333 });
 }
